dfd10b67839a1c7d263bc2dc4d254733c13aad9d
arshia <arshia.karimi@gmail.com>
2024-02-17 17:52:34 +0330

    end

--- javascript.js
+++ javascript.js
@@ -1,16 +1,21 @@
-const button = document.querySelector("button");
-const in1 = document.querySelector("#in1");
-const in2 = document.querySelector("#in2");
-const in3 = document.querySelector("#in3");
+const in1 = document.getElementById("in1");
+const in2 = document.getElementById("in2");
+const in3 = document.getElementById("in3");
+const btn = document.querySelector("button");
 const p = document.querySelector("p");
 
-button.addEventListener("click", function () {
+btn.addEventListener("click", () => {
   if (in1.value && in2.value && in3.value) {
     if (Number(in1.value) >= Number(in2.value) + Number(in3.value)) {
-      let percentage =
-        100 *
-        ((3 * Number(in2.value) - Number(in3.value)) / (Number(in1.value) * 3));
-      p.innerHTML = parseFloat(percentage).toFixed(2) + "%";
+      if (Number(in1.value == 0)) {
+        alert("اشتباه است.");
+      } else {
+        let percentage =
+          100 *
+          ((3 * Number(in2.value) - Number(in3.value)) /
+            (Number(in1.value) * 3));
+        p.innerHTML = percentage.toFixed(2) + "%";
+      }
     } else {
       alert("مجموع سوالات درست و غلط بیشتر از تعداد کل سوالات است.");
     }
